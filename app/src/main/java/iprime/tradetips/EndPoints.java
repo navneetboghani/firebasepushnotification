0fd0295de58ed397b27368b18b151fdd48550a6a
Navneet Boghani <adsum.originator@gmail.com>
2018-08-06 16:16:52 +0530

    init code commit
    add new code

---
+++ app/src/main/java/iprime/tradetips/EndPoints.java
@@ -0,0 +1,12 @@
+package iprime.tradetips;
+
+/**
+ * Created by Belal on 04/11/16.
+ */
+
+public class EndPoints {
+    public static final String URL_REGISTER_DEVICE = "http://tradeadmin.iprimetechnosoft.com/RegisterDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "http://tradeadmin.iprimetechnosoft.com/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "http://tradeadmin.iprimetechnosoft.com/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = "http://tradeadmin.iprimetechnosoft.com/GetRegisteredDevices.php";
+}
